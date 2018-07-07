b0a0a4108d0eabec2fb1f8c2bba185585907bd63
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 04:08:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,8 +68,9 @@ def self.first_student_in_grade_10
   sql = <<~SQL
     SELECT * FROM students
     WHERE grade = 10
+    LIMIT 1
     SQL
-    DB[:conn].execute(sql).first
+    DB[:conn].execute(sql)
   end
 
   def save

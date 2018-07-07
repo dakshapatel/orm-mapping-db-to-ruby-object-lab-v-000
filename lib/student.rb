cd5701089774e8db65c292fd9870518a38d1e50d
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 03:43:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,14 +52,16 @@ class Student
       DB[:conn].execute(sql)
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(grade)
     sql = <<~SQL
       SELECT * FROM students
       WHERE grade = 10
       GROUP BY name
       LIMIT X
       SQL
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql, grade).map do |row|
+     self.new_from_db(row)
+   end.first
   end
 
 

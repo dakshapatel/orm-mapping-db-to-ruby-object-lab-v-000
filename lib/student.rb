4c2213925447c52ab212617b6e86367d1fd46018
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 04:04:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,9 +58,8 @@ class Student
       SELECT * FROM students
       WHERE grade = 10
       SQL
-      DB[:conn].execute(sql, grade).map do |row|
-        self.new_from_db(row)
-      end.first
+      DB[:conn].execute(sql).first
+      
 
   end
 

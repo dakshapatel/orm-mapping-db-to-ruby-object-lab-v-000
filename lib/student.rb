11be8d6af95e292d36a73d2dc02e0e29c28d2032
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 04:40:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,8 +58,9 @@ class Student
       LIMIT ?
 
       SQL
-      DB[:conn].execute(sql)
-      
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end.first
   end
 
 

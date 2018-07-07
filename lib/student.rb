9e39b684db020b025bf977347557dbdf9b33fe4e
Daksha Patel <dakshapatel100@gmail.com>
2018-07-07 03:26:43 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,12 @@ class Student
     # return a new instance of the Student class
 
   def self.count_all_students_in_grade_9
-
+    sql = <<~SQL
+      SELECT * FROM Students
+      WHERE grade = 9
+      SQL
+      DB[:conn].execute(sql)
+      
   end
 
 
